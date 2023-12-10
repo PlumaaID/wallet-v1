9620afdc51d2c339002f81066b5cb152060e416f
ernestognw <ernestognw@gmail.com>
2023-12-09 20:50:18 -0600

    :construction_worker: Attempt to fix CI

--- scripts/modulus.sh
+++ scripts/modulus.sh
@@ -2,4 +2,4 @@
 
 set -euo pipefail
 
-openssl rsa -pubin -outform DER -in keys/$1/private.pem -noout -modulus | sed 's/Modulus=//g'
+openssl rsa -pubin -outform DER -in ./keys/$1/public.pem -noout -modulus | sed 's/Modulus=//g' | tr -d \\n
